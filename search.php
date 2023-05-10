8d1c02a8bb0d964df13b0d044c01577209aec6c1
Kirill <133170635+KirillDEVVENDAS@users.noreply.github.com>
2023-05-11 02:36:40 +0300

    search.php

--- search.php
+++ search.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * The template for displaying search results pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
- *
- * @package prolancer
- */
+
 
 get_header();
 
